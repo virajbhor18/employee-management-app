198bbe362e4091ae5f550cc602f63f6104f0946f
Viraj Bhor <virajbhor18@gmail.com>
2026-07-06 11:08:33 +0530

    Fix Android application ID to com.virajbhor.employeemanagementapp

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.hello_flutter"
+    namespace = "com.virajbhor.employeemanagementapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hello_flutter"
+        applicationId = "com.virajbhor.employeemanagementapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/virajbhor/employeemanagementapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/virajbhor/employeemanagementapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hello_flutter
+package com.virajbhor.employeemanagementapp
 
 import io.flutter.embedding.android.FlutterActivity
 
