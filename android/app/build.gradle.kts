e910cf0899cb181495e23e33b5a8ba9448b979c0
Viraj Bhor <virajbhor18@gmail.com>
2026-07-14 14:41:05 +0530

    Configure Firebase project and Android Gradle setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -32,6 +33,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
 }
 
 kotlin {
