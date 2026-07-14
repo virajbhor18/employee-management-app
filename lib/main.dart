a998575f7752e1eb7f7e353ba6a1916a8fcf4c55
Viraj Bhor <virajbhor18@gmail.com>
2026-07-14 15:46:06 +0530

    Initialize Firebase in app, add firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'views/auth/login_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,4 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
